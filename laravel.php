70a90f03f5ece885603e1170e0a91e88f268f39a
Metourni Noureddine <en_metourni@esi.dz>
2018-03-24 15:05:28 +0100

    Update laravel.php

--- laravel.php
+++ laravel.php
@@ -3,10 +3,11 @@
 * @Auther : Metourni Noureddine
 *
 *
-* @Description : 
-				[
-					($ cmd) => mean that is a terminal cmd,
-				]
+* @Description :
+*	[
+*		($ cmd) => a terminal command,
+*	]
+*
 */
 
 1- Installation :
@@ -20,28 +21,19 @@
 
 2- migration : 
 	$ php artisan migrate
-
-	if there is this problème : '#1071 - Specified key was too long; max key length is 767 bytes'
+	- If there is this problème : '#1071 - Specified key was too long; max key length is 767 bytes'
 		1- go to : 'AppServiceProvider.php'
 		2- put this code :
-		
-		'
+		"
 			use Illuminate\Support\Facades\Schema;
 
 			public function boot()
 			{
     			Schema::defaultStringLength(191);
 			}
-		'
-	end if;
-
-	if you have MacOS or Unix you should do this :
+		"
+	- If you have MacOS or Unix you should do this :
 		1- go to /config/database.php
 		2- put this code in mysql driver options :
-		'
-			'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
-
-		'
-	endif;
-
+			'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),```
 
